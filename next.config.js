6b48aa1c70666454765912972f2dd2ecd6c22300
Thomas Guillet <thomas@palya.fr>
2025-04-08 17:34:41 +0200

    Add basePath in next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProduction = "production" === process.env.NODE_ENV;
+
 module.exports = ({
+  basePath: isProduction ? "/nextjs-netlify-blog-template" : "",
   output: "export",
   pageExtensions: ["tsx"],
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
