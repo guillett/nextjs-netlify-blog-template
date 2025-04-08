056e12913975f3da9d0764f0330614b2f8ab38a5
Thomas Guillet <thomas@palya.fr>
2025-04-08 17:23:01 +0200

    Add next-mdx-remote react-schemaorg

--- src/pages/posts/[post].tsx
+++ src/pages/posts/[post].tsx
@@ -1,7 +1,6 @@
 import { GetStaticProps, GetStaticPaths } from "next";
-import renderToString from "next-mdx-remote/render-to-string";
-import { MdxRemote } from "next-mdx-remote/types";
-import hydrate from "next-mdx-remote/hydrate";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote, type MDXRemoteSerializeResult } from "next-mdx-remote";
 import matter from "gray-matter";
 import { fetchPostContent } from "../../lib/posts";
 import fs from "fs";
@@ -9,9 +8,9 @@ import yaml from "js-yaml";
 import { parseISO } from 'date-fns';
 import PostLayout from "../../components/PostLayout";
 
-import InstagramEmbed from "react-instagram-embed";
-import YouTube from "react-youtube";
-import { TwitterTweetEmbed } from "react-twitter-embed";
+//import InstagramEmbed from "react-instagram-embed";
+//import YouTube from "react-youtube";
+//import { TwitterTweetEmbed } from "react-twitter-embed";
 
 export type Props = {
   title: string;
@@ -20,10 +19,10 @@ export type Props = {
   tags: string[];
   author: string;
   description?: string;
-  source: MdxRemote.Source;
+  source: MDXRemoteSerializeResult;
 };
 
-const components = { InstagramEmbed, YouTube, TwitterTweetEmbed };
+const components = {}// InstagramEmbed, YouTube, TwitterTweetEmbed };
 const slugToPostContent = (postContents => {
   let hash = {}
   postContents.forEach(it => hash[it.slug] = it)
@@ -39,7 +38,6 @@ export default function Post({
   description = "",
   source,
 }: Props) {
-  const content = hydrate(source, { components })
   return (
     <PostLayout
       title={title}
@@ -49,7 +47,7 @@ export default function Post({
       author={author}
       description={description}
     >
-      {content}
+      <MDXRemote {...source} components={components} />
     </PostLayout>
   )
 }
@@ -68,7 +66,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { content, data } = matter(source, {
     engines: { yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object }
   });
-  const mdxSource = await renderToString(content, { components, scope: data });
+  const mdxSource = await serialize(content, { scope: data });
   return {
     props: {
       title: data.title,
